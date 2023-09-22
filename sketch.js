0edc9d1f1b6a56c6a0e58362935a327871dbbfe5
Drax <143183749+Sviercoski@users.noreply.github.com>
2023-09-22 08:35:07 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+background("red");}
+
+function draw() {
+  
+  
+  stroke('blue')
+  fill("blue")
+  
+ //console.log (mouseIsPressed);
+  
+  if(mouseIsPressed) {square(mouseX, mouseY, 20,20)};
+  
+ 
+  
+}
